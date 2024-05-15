9059e83438bdb5727953894542c59035c2c42a7a
mikulickata <142401283+mikulickata@users.noreply.github.com>
2024-05-15 09:28:41 +0200

    add news

---
+++ Assets/Scripts/ReplaceObject.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class ReplaceObject : MonoBehaviour
+{
+    public GameObject[] objects; // Niz objekata koji æe biti upravljani
+    public Vector3 offsetFromParent = Vector3.zero; // Pomak od roditeljskog GameObjecta
+
+    private void Start()
+    {
+        // Na poèetku iskljuèujemo sve objekte
+        foreach (GameObject obj in objects)
+        {
+            obj.SetActive(false);
+        }
+    }
+
+    // Metoda za prikazivanje odreğenog objekta
+    public void ShowObject(int index)
+    {
+
+        // Prikazujemo samo odabrani objekt ako je index validan
+        if (index >= 0 && index < objects.Length)
+        {
+            objects[index].SetActive(true);
+            // Postavljamo objekt na poziciju roditeljskog GameObjecta uz dodatak offseta
+            objects[index].transform.position = transform.position + offsetFromParent;
+        }
+    }
+
+
+    // Metoda za skrivanje pojedinaènih objekata
+    public void HideObject()
+    {
+        // Iskljuèujemo sve objekte
+        foreach (GameObject obj in objects)
+        {
+            obj.SetActive(false);
+        }
+    }
+}
