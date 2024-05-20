0e5f098a933e374cc9709006282f2a4aeaa81ec7
mikulickata <142401283+mikulickata@users.noreply.github.com>
2024-05-20 12:30:13 +0200

    add buttins, and sizing few object, need scaling

--- Library/Bee/artifacts/UWPPlayerBuildProgram/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
+++ Library/Bee/artifacts/UWPPlayerBuildProgram/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -12,14 +12,32 @@
 
 
 
+extern void NextText_Start_m0D6C9DDC3C70AC4D5F338AD8F9C7274E625495E1 (void);
+extern void NextText_ShowCanvas1_mF4AB91D5CA74EF142D353CB2745C7C9285CC50E3 (void);
+extern void NextText_ShowCanvas2_m7374E8B950A4407FA4B1012EEE03CA18CD23E39E (void);
+extern void NextText_ShowCanvas3_m172F603EC66652CD9DCB7D24999AF999C8AADA61 (void);
+extern void NextText_OnNextButtonCanvas1_m147B3269234A40C9FF2ED449ECB2104494C70997 (void);
+extern void NextText_OnNextButtonCanvas2_m161E0B91F29EFD4369465061BBF9DC14A8200143 (void);
+extern void NextText_OnPreviousButtonCanvas2_m51A2D0DD192C93BEA44827739FB2C5D83D238368 (void);
+extern void NextText_OnPreviousButtonCanvas3_mFD3D6E0C6E65CC57E1FE4C2F46B9FE7FE42C5C69 (void);
+extern void NextText__ctor_m9ECF31F7D2658BC233949C815A15A4B6379B4DB8 (void);
 extern void ReplaceObject_Start_m8358B9536DB7D0590FD3C913CDC60AF8340ADE12 (void);
 extern void ReplaceObject_ShowObject_m84BB8FB4CC37F3ECD677BBC3A2DB2F6C9AA19C17 (void);
 extern void ReplaceObject_HideObject_m241E030FE90FEFA05D2C4F39E307025F30A0CAE4 (void);
 extern void ReplaceObject__ctor_m10EBDB897A18FF76EE6244B4A65AA4A376B4B85B (void);
 extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
 extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
-static Il2CppMethodPointer s_methodPointers[6] = 
+static Il2CppMethodPointer s_methodPointers[15] = 
 {
+	NextText_Start_m0D6C9DDC3C70AC4D5F338AD8F9C7274E625495E1,
+	NextText_ShowCanvas1_mF4AB91D5CA74EF142D353CB2745C7C9285CC50E3,
+	NextText_ShowCanvas2_m7374E8B950A4407FA4B1012EEE03CA18CD23E39E,
+	NextText_ShowCanvas3_m172F603EC66652CD9DCB7D24999AF999C8AADA61,
+	NextText_OnNextButtonCanvas1_m147B3269234A40C9FF2ED449ECB2104494C70997,
+	NextText_OnNextButtonCanvas2_m161E0B91F29EFD4369465061BBF9DC14A8200143,
+	NextText_OnPreviousButtonCanvas2_m51A2D0DD192C93BEA44827739FB2C5D83D238368,
+	NextText_OnPreviousButtonCanvas3_mFD3D6E0C6E65CC57E1FE4C2F46B9FE7FE42C5C69,
+	NextText__ctor_m9ECF31F7D2658BC233949C815A15A4B6379B4DB8,
 	ReplaceObject_Start_m8358B9536DB7D0590FD3C913CDC60AF8340ADE12,
 	ReplaceObject_ShowObject_m84BB8FB4CC37F3ECD677BBC3A2DB2F6C9AA19C17,
 	ReplaceObject_HideObject_m241E030FE90FEFA05D2C4F39E307025F30A0CAE4,
@@ -27,8 +45,17 @@ static Il2CppMethodPointer s_methodPointers[6] =
 	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
 	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
 };
-static const int32_t s_InvokerIndices[6] = 
+static const int32_t s_InvokerIndices[15] = 
 {
+	8789,
+	8789,
+	8789,
+	8789,
+	8789,
+	8789,
+	8789,
+	8789,
+	8789,
 	8789,
 	6884,
 	8789,
@@ -40,7 +67,7 @@ IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
 const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
 {
 	"Assembly-CSharp.dll",
-	6,
+	15,
 	s_methodPointers,
 	0,
 	NULL,
